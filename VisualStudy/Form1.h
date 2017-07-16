b25019ceda0cb543b7cc8cdeacbbc0b5f31f1c7d
alasoso <sodunux@163.com>
2017-07-16 22:31:07 +0800

    2017-07-16

--- VisualStudy/Form1.h
+++ VisualStudy/Form1.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <tchar.h>
 namespace VisualStudy {
 
 	using namespace System;
@@ -34,12 +34,84 @@ namespace VisualStudy {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TabControl^  Pg7816;
+	protected: 
+
+
+	private: System::Windows::Forms::TabPage^  tabPage2;
+	private: System::Windows::Forms::TabPage^  Pg14443;
+	private: System::Windows::Forms::Button^  P14443_Btn1;
+
+
+
+	private: System::Windows::Forms::SplitContainer^  splitContainer1;
+	private: System::Windows::Forms::RichTextBox^  P14443_Text1;
+
+
+
+	private: Microsoft::VisualBasic::PowerPacks::ShapeContainer^  shapeContainer2;
+	private: Microsoft::VisualBasic::PowerPacks::LineShape^  lineShape2;
+	private: System::Windows::Forms::ComboBox^  P14443_Cbox1;
+	private: System::Windows::Forms::Label^  P14443_Lab1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	private: System::ComponentModel::IContainer^  components;
+
+
+
+
+
+
+
+
+	protected: 
+
+	protected: 
+
+
 
 	private:
 		/// <summary>
 		/// 必需的设计器变量。
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -49,20 +121,134 @@ namespace VisualStudy {
 		void InitializeComponent(void)
 		{
 			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(Form1::typeid));
+			this->splitContainer1 = (gcnew System::Windows::Forms::SplitContainer());
+			this->P14443_Text1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->Pg7816 = (gcnew System::Windows::Forms::TabControl());
+			this->Pg14443 = (gcnew System::Windows::Forms::TabPage());
+			this->P14443_Btn1 = (gcnew System::Windows::Forms::Button());
+			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
+			this->P14443_Lab1 = (gcnew System::Windows::Forms::Label());
+			this->shapeContainer2 = (gcnew Microsoft::VisualBasic::PowerPacks::ShapeContainer());
+			this->lineShape2 = (gcnew Microsoft::VisualBasic::PowerPacks::LineShape());
+			this->P14443_Cbox1 = (gcnew System::Windows::Forms::ComboBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->splitContainer1))->BeginInit();
+			this->splitContainer1->Panel1->SuspendLayout();
+			this->splitContainer1->Panel2->SuspendLayout();
+			this->splitContainer1->SuspendLayout();
+			this->Pg7816->SuspendLayout();
+			this->Pg14443->SuspendLayout();
 			this->SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			resources->ApplyResources(this->splitContainer1, L"splitContainer1");
+			this->splitContainer1->Name = L"splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this->splitContainer1->Panel1->Controls->Add(this->P14443_Text1);
+			this->splitContainer1->Panel1->Controls->Add(this->shapeContainer2);
+			this->splitContainer1->Panel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Form1::splitContainer1_Panel1_Paint);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this->splitContainer1->Panel2->Controls->Add(this->P14443_Cbox1);
+			this->splitContainer1->Panel2->Controls->Add(this->P14443_Lab1);
+			// 
+			// P14443_Text1
+			// 
+			resources->ApplyResources(this->P14443_Text1, L"P14443_Text1");
+			this->P14443_Text1->Name = L"P14443_Text1";
+			// 
+			// Pg7816
+			// 
+			this->Pg7816->Controls->Add(this->Pg14443);
+			this->Pg7816->Controls->Add(this->tabPage2);
+			resources->ApplyResources(this->Pg7816, L"Pg7816");
+			this->Pg7816->Name = L"Pg7816";
+			this->Pg7816->SelectedIndex = 0;
+			// 
+			// Pg14443
+			// 
+			this->Pg14443->Controls->Add(this->splitContainer1);
+			this->Pg14443->Controls->Add(this->P14443_Btn1);
+			resources->ApplyResources(this->Pg14443, L"Pg14443");
+			this->Pg14443->Name = L"Pg14443";
+			this->Pg14443->UseVisualStyleBackColor = true;
+			this->Pg14443->Click += gcnew System::EventHandler(this, &Form1::Pg14443_Click);
+			// 
+			// P14443_Btn1
+			// 
+			resources->ApplyResources(this->P14443_Btn1, L"P14443_Btn1");
+			this->P14443_Btn1->Name = L"P14443_Btn1";
+			this->P14443_Btn1->UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			resources->ApplyResources(this->tabPage2, L"tabPage2");
+			this->tabPage2->Name = L"tabPage2";
+			this->tabPage2->UseVisualStyleBackColor = true;
+			// 
+			// P14443_Lab1
+			// 
+			resources->ApplyResources(this->P14443_Lab1, L"P14443_Lab1");
+			this->P14443_Lab1->Name = L"P14443_Lab1";
+			// 
+			// shapeContainer2
+			// 
+			resources->ApplyResources(this->shapeContainer2, L"shapeContainer2");
+			this->shapeContainer2->Name = L"shapeContainer2";
+			this->shapeContainer2->Shapes->AddRange(gcnew cli::array< Microsoft::VisualBasic::PowerPacks::Shape^  >(1) {this->lineShape2});
+			this->shapeContainer2->TabStop = false;
+			// 
+			// lineShape2
+			// 
+			this->lineShape2->Name = L"lineShape2";
+			resources->ApplyResources(this->lineShape2, L"lineShape2");
+			// 
+			// P14443_Cbox1
+			// 
+			resources->ApplyResources(this->P14443_Cbox1, L"P14443_Cbox1");
+			this->P14443_Cbox1->FormattingEnabled = true;
+			this->P14443_Cbox1->Items->AddRange(gcnew cli::array< System::Object^  >(2) {resources->GetString(L"P14443_Cbox1.Items"), 
+				resources->GetString(L"P14443_Cbox1.Items1")});
+			this->P14443_Cbox1->Name = L"P14443_Cbox1";
 			// 
 			// Form1
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 12);
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(442, 371);
-			this->Icon = (cli::safe_cast<System::Drawing::Icon^  >(resources->GetObject(L"$this.Icon")));
+			this->AllowDrop = true;
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
+			resources->ApplyResources(this, L"$this");
+			this->AutoValidate = System::Windows::Forms::AutoValidate::EnableAllowFocusChange;
+			this->Controls->Add(this->Pg7816);
+			this->HelpButton = true;
 			this->Name = L"Form1";
-			this->Text = L"Form1";
+			this->splitContainer1->Panel1->ResumeLayout(false);
+			this->splitContainer1->Panel2->ResumeLayout(false);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->splitContainer1))->EndInit();
+			this->splitContainer1->ResumeLayout(false);
+			this->Pg7816->ResumeLayout(false);
+			this->Pg14443->ResumeLayout(false);
+			this->Pg14443->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
-	};
+
+
+
+private: System::Void Pg14443_Click(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void maskedTextBox1_MaskInputRejected(System::Object^  sender, System::Windows::Forms::MaskInputRejectedEventArgs^  e) {
+		 }
+private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void label1_Click_1(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void label1_Click_2(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void splitContainer1_Panel1_Paint(System::Object^  sender, System::Windows::Forms::PaintEventArgs^  e) {
+		 }
+};
 }
 
